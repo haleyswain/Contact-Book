b81b139184e7f0c7213902ba64a4a3b20258e6ea
Haley Swain and Skye Atwood <skye@>
2016-12-01 12:00:37 -0800

    route saved contacts to contact page via sinatra

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ get("/") do
   erb(:index)
 end
 
-get("/") do
+post("/success") do
   first_name = params.fetch("firstName")
   last_name = params.fetch("lastName")
   job_title = params.fetch("jobTitle")
@@ -15,4 +15,9 @@ get("/") do
   persons = Contact.new(first_name, last_name, job_title, company)
   persons.save()
   erb(:success)
+end
+
+get("/contacts") do
+  @persons = Contact.all()
+  erb(:contacts)
 end
--- lib/contacts.rb
+++ lib/contacts.rb
@@ -3,11 +3,11 @@ class Contact
 
   attr_reader(:first_name, :last_name, :job_title, :company)
 
-  define_method(:initialize) do |info|
-    @first_name = info.fetch(:first_name)
-    @last_name = info.fetch(:last_name)
-    @job_title = info.fetch(:job_title)
-    @company = info.fetch(:company)
+  define_method(:initialize) do |first, last, job, company|
+    @first_name = first
+    @last_name = last
+    @job_title = job
+    @company = company
   end
 
   define_singleton_method(:all) do
